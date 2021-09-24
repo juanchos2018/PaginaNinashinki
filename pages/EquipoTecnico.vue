84d90fe81e02cc3821704db92ad7bac023caf416
juanchos2018 <jcarlossenati@gmail.com>
2021-09-24 13:03:09 -0500

    avance nuxt equipo

--- pages/EquipoTecnico.vue
+++ pages/EquipoTecnico.vue
@@ -7,7 +7,85 @@
 
         <section id="home">
           <video-banner :img="defaulimg" />
+          <!-- <video-banner /> -->
         </section>
+
+        <section>
+          <div class="">
+            <v-container class="fixed-width">
+              <div class="massonry">
+                <u-animate-container>
+                  <v-row>
+                    <v-card class="" max-width="434" tile>
+                      
+                      <v-img
+                        height="100%"
+                        src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
+                      >
+                      
+                        <v-row align="end" class="fill-height">
+
+                          <v-col align-self="start" class="pa-0" cols="6">
+                            <v-avatar
+                              class="profile"
+                              color="grey"
+                              size="164"
+                              tile
+                            >
+                              <v-img
+                                src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"
+                                  alt="Avatar"
+                              ></v-img>
+                            </v-avatar>
+                         
+                          </v-col>
+
+                          <v-col >
+                            <v-list-item color="rgba(0, 0, 0, .4)" dark>
+                              <v-list-item-content>
+                                <v-list-item-title class="text-h6">
+                                  Marcus Obrien
+                                </v-list-item-title>
+                                <v-list-item-subtitle
+                                  >Network Engineer</v-list-item-subtitle
+                                >
+                              </v-list-item-content>
+                            </v-list-item>
+                          </v-col>
+                          
+                           <v-col  align-self="top"  >
+                            <v-list-item color="rgba(0, 0, 0, .4)" dark>
+                              <v-list-item-content>
+                                <v-list-item-title class="text-h6">
+                                  Mdesdes
+                                </v-list-item-title>
+                                <v-list-item-subtitle
+                                  >Network Engineer</v-list-item-subtitle
+                                >
+                              </v-list-item-content>
+                            </v-list-item>
+                          </v-col>
+
+                        </v-row>
+
+
+                     
+
+                      </v-img>
+                      
+                    </v-card>
+                  </v-row>
+                </u-animate-container>
+              </div>
+            </v-container>
+          </div>
+        </section>
+
+        <br /><br /><br />
+
+        <br /><br /><br />
+
+        <br /><br />
 
         <!-- <section
           id="about"
@@ -15,8 +93,6 @@
         >
           <about />
         </section> -->
-
-        <dots-parallax />
       </div>
 
       <hidden point="mdDown">
@@ -27,47 +103,8 @@
   </div>
 </template>
 
-<style scoped lang="scss">
-@import '~/assets/styles';
-
-@function section-margin($margin) {
-  @return $margin * 20;
-}
-.main-wrap {
-  position: relative;
-  width: 100%;
-  overflow: hidden;
-  @include palette-text-primary;
-  .theme--dark & {
-    background-color: $dark-background-default;
-  }
-  .theme--light & {
-    background-color: $light-background-paper;
-  }
-}
-.space-top {
-  margin-top: section-margin($spacing1);
-  @include breakpoints-down(md) {
-    margin-top: section-margin(6px);
-  }
-  @include breakpoints-down(xs) {
-    margin-top: section-margin($spacing1 / 2);
-  }
-}
-.space-bottom-short {
-  margin-bottom: section-margin($spacing1 / 2);
-}
-.space-top-short {
-  margin-top: section-margin($spacing1 / 2);
-}
-.container-wrap {
-  @include breakpoints-up(md) {
-    margin-top: $spacing6;
-  }
-  section {
-    position: relative;
-  }
-}
+<style lang="scss" scoped>
+@import './about-style.scss';
 </style>
 
 <script>
@@ -75,14 +112,18 @@ import Header from '~/components/Header'
 import SquareParallax from '~/components/Parallax/Square'
 import VideoBanner from '~/components/VideoBanner'
 import About from '~/components/About'
-import Services from '~/components/Services'
-import Expertise from '~/components/Expertise'
+//import Services from '~/components/Services'
+import CallAction from '~/components/CallAction'
 import Footer from '~/components/Footer'
 import Hidden from '~/components/Hidden'
 import PageNav from '~/components/PageNav'
 //import Notification from '~/components/Notification'
 import DotsParallax from '~/components/Parallax/Dots'
 import brand from '~/static/text/brand'
+import imgAPI from '~/static/images/imgAPI'
+//import Mision from '@/components/Mision/Mision'
+//import Necesidad from '@/components/Necesidad/Necesidad'
+//import TestiCard from '@/components/Cards/Vision'
 
 export default {
   components: {
@@ -91,16 +132,11 @@ export default {
     DotsParallax,
     VideoBanner,
     About,
-    Services,
-    Expertise,
+    CallAction,
     PageNav,
     Hidden,
+
     'main-footer': Footer,
-  },
-  data() {
-    return {
-      defaulimg: './images/logojuvenil.png',
-    }
   },
   computed: {
     isTablet() {
@@ -111,13 +147,30 @@ export default {
     isMobile() {
       return this.$mq === 'smDown' || this.$mq === 'xsDown'
     },
+    mdUp() {
+      return this.$mq === 'mdDown' || this.$mq === 'lgDown' || this.$mq === 'xl'
+    },
+    isDesktop() {
+      return this.$mq === 'mdDown' || this.$mq === 'lgDown' || this.$mq === 'xl'
+    },
+  },
+  data() {
+    return {
+      defaulimg: './images/logojuvenil.png',
+      imgAPI: imgAPI,
+      imagnes_uno: '/images/DSC00695.JPG',
+      imagnes_dos: '/images/isc lima - Isaac Peñaherrera.jpg',
+      objetivo:
+        'Replantear los principios de las identidades juveniles hacia la toma de decisiones con respecto a la situación que atraviesa sus barrios, mediante  las prácticas y saberes de las culturas urbanas y ancestrales.',
+    }
   },
   mounted() {
     this.$vuetify.theme.dark = true
   },
+
   head() {
     return {
-      title: 'NinashunKu' + ' - Equipo Tecnico',
+      title: 'NinashunKu' + ' - Conocenos',
     }
   },
 }
